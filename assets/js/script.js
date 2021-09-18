8918b648708f034b92cde5d5960abd81eba77195
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-09-18 15:38:10 -0400

    added function to check each letter pressed and add it to the word

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@ var timerCounter = $(".timer-count");
 var wordBlanks = $(".word-blanks");
 
 var secondsLeft = 10;
+var randomWord = "";
+var randomWordArr = [];
+var underscore = [];
+var numOfBlanks = 0;
 
 function startTimer() {    
     var timer = setInterval(() => {
@@ -14,30 +18,57 @@ function startTimer() {
     }, 1000);
 }
 
-startButton.on("click", startGame);
-
 var words = ["blue", "green", "purple", "orange", "red", "yellow", "brown"];
 
 function startGame() {
     startTimer();
-    var underscore = [""];
-    var randomWord = words[Math.floor(Math.random() * words.length)];
-    // console.log(randomWord);
-    for (let i = 0; i < randomWord.length; i++) {
+    
+    randomWord = words[Math.floor(Math.random() * words.length)];
+    randomWordArr = randomWord.split("");
+    numOfBlanks = randomWordArr.length;
+    underscore = []
+    console.log(randomWordArr)
+    for (let i = 0; i < numOfBlanks; i++) {
         underscore.push("_");
     }
-    underscore = underscore.join(" ")
-    wordBlanks.text(underscore)
+    wordBlanks.text(underscore.join(" "))
+}
 
+function checkLetter(letter) {
+    // console.log(letter)
+    var letterInWord = false;
+    for (var i = 0; i < numOfBlanks; i++) {
+        console.log(randomWord[i])
+        if (randomWord[i] === letter) {
+            letterInWord = true;
+            console.log(letter)
+        }
+    }
+    if (letterInWord) {
+        for (var j = 0; j < numOfBlanks; j++) {
+            if (randomWord[j] === letter) {
+                underscore[j] = letter;
+                console.log(letter)
+            }        
+        }
+        console.log(underscore)
+        console.log(underscore.join(" "));
+        wordBlanks.text(underscore.join(" "))
+    }
 }
 
 document.addEventListener("keydown", function (e) {
+    if (secondsLeft === 0) {
+        return;
+    }
     var key = e.key.toLowerCase();
     var alphabet = "abcdefghijklmnopqrstuvwxyz ".split("");
     // console.log(alphabet)
     if (alphabet.includes(key)) {
         var letterGuessed = e.key;
         console.log(letterGuessed)
-        
+        checkLetter(letterGuessed);
     }
 })
+
+startButton.on("click", startGame);
